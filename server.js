b1a343bab434b487cf4aed52a81956f8b9333b34
HassanBahati <mukisabahati@gmail.com>
2021-07-03 13:45:08 +0300

    make it better

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 5000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.send("this is the api root handler");
+});
+
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
